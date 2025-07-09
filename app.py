6040fb21d18dbb048baeda5d5ffc5baf756b0334
Abtin Khashayar <akhashayar26@berkeley.edu>
2025-07-08 23:33:29 -0700

    Created form with flask

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import os
 from dotenv import load_dotenv
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_behind_proxy import FlaskBehindProxy
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm
 app = Flask(__name__)
 proxied = FlaskBehindProxy(app)  ## add this line
 load_dotenv() # load from our .env file
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') # here we get our key stored in our .env file!
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS']= False # this is so that we don't get a debug confirmation message during url redirects
 app.debug = True
 toolbar = DebugToolbarExtension(app)
 
@@ -14,6 +16,17 @@ toolbar = DebugToolbarExtension(app)
 @app.route("/home")
 def home():
     return render_template('home.html', subtitle='Home Page', text= 'Welcome to NewsLink!')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit(): # checks if entries are valid
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home')) # if so - send to home page
+    return render_template('registration.html', title='Register', form=form)
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5001)
+
 
